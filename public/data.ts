d38bef0017e0f644380b587b77f022eaf154ada4
Kaka Abdeel Parubak <kakaparubak0420@gmail.com>
2026-05-24 14:48:34 +0700

    fix: standardize image object syntax in data.ts

--- public/data.ts
+++ public/data.ts
@@ -1,38 +1,38 @@
 export const statsImages = [
   {
-    src: "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
   },
   {
-    src: "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
   },
   {
-    src: "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-  },
-];
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
+  }
+]
 export const rcheckImages = [
   {
-    src: "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
   },
   {
-    src: "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
   },
   {
-    src: "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
   },
   {
-    src: "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
   },
   {
-    src: "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-  },
-];
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
+  }
+]
 
 export const projects = [
   {
-    id: 1,
-    name: "Project 1",
-    date: "25/12/07",
-    imgs: [
+    "id": 1,
+    "name": "Project 1",
+    "date": "25/12/07",
+    "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
@@ -41,91 +41,81 @@ export const projects = [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
   },
   {
-    id: 2,
-    name: "Long Project Name 1",
-    date: "25/12/07",
-    imgs: [
+    "id": 2,
+    "name": "Long Project Name 1",
+    "date": "25/12/07",
+    "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
   },
   {
-    id: 3,
-    name: "Watsap Brodie",
-    date: "25/12/07",
-    imgs: [
+    "id": 3,
+    "name": "Watsap Brodie",
+    "date": "25/12/07",
+    "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
   },
   {
-    id: 4,
-    name: "Satria MPRUY GEMING",
-    date: "25/12/07",
-    imgs: [
+    "id": 4,
+    "name": "Satria MPRUY GEMING",
+    "date": "25/12/07",
+    "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
   },
   {
-    id: 5,
-    name: "Satria MPRUY GEMING",
-    date: "25/12/07",
-    imgs: [
+    "id": 5,
+    "name": "Satria MPRUY GEMING",
+    "date": "25/12/07",
+    "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
   },
   {
-    id: 6,
-    name: "Satria MPRUY GEMING",
-    date: "25/12/07",
-    imgs: [
+    "id": 6,
+    "name": "Satria MPRUY GEMING",
+    "date": "25/12/07",
+    "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
   },
   {
-    id: 7,
-    name: "Satria MPRUY GEMING",
-    date: "25/12/07",
-    imgs: [
+    "id": 7,
+    "name": "Satria MPRUY GEMING",
+    "date": "25/12/07",
+    "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
   },
   {
-    id: 8,
-    name: "Satria MPRUY GEMING",
-    date: "25/12/07",
-    imgs: [
+    "id": 8,
+    "name": "Satria MPRUY GEMING",
+    "date": "25/12/07",
+    "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
-  },
-  {
-    id: 9,
-    name: "Satria MPRUY GEMING",
-    date: "25/12/07",
-    imgs: [
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
-      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg",
-    ],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus.",
-  },
-];
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
+  }
+]
