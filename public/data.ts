2a267d2a438593ab70c4dfd5c17e28b2e04b95a6
Kaka Abdeel Parubak <kakaparubak0420@gmail.com>
2026-05-24 17:50:38 +0700

    fix: update asset loading error handling and improve project data structure

--- public/data.ts
+++ public/data.ts
@@ -1,36 +1,26 @@
-export const statsImages = [
-  {
-    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
-  },
-  {
-    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
-  },
-  {
-    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
-  }
-]
+
 export const rcheckImages = [
   {
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL0599_vgrzr0.jpg"
+  },
+  {
     "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
+  },
+  {
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016719/_RZL4208_jxbzrm.jpg"
+  },
+  {
+    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL3276_flofxk.jpg"
   },
   {
     "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
-  },
-  {
-    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
-  },
-  {
-    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
-  },
-  {
-    "src": "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg"
   }
 ]
 
 export const projects = [
   {
     "id": 1,
-    "name": "Project 1",
+    "name": "Halo men ",
     "date": "25/12/07",
     "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
@@ -67,7 +57,7 @@ export const projects = [
   },
   {
     "id": 4,
-    "name": "Satria MPRUY GEMING",
+    "name": "HALOOO",
     "date": "25/12/07",
     "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
@@ -77,7 +67,7 @@ export const projects = [
   },
   {
     "id": 5,
-    "name": "Satria MPRUY GEMING",
+    "name": "YO WATSAPPP",
     "date": "25/12/07",
     "imgs": [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016716/_RZL0332_cru3kx.jpg",
@@ -117,5 +107,17 @@ export const projects = [
       "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779016714/_RZL8664_eu59gw.jpg"
     ],
     "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis sem vel leo fermentum, at ultricies nisi aliquet. Sed vestibulum vel sem non egestas. Nullam a lacus metus."
+  },
+  {
+    "id": 9,
+    "name": "90s Intimate (MLTR)",
+    "date": "08/02/2026",
+    "imgs": [
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779017266/IMG_8797.JPG_n229yx.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779017258/90sintimate-1334_mdniro.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779017255/90s-TALE_SOCIETY-9340_ibhrbw.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779017261/90s-TALE_SOCIETY-03425_hrt7r0.jpg",
+      "https://res.cloudinary.com/dipyszxjg/image/upload/q_auto/f_auto/v1779017263/90s-TALE_SOCIETY-8832_kd47m6.jpg"
+    ]
   }
 ]
